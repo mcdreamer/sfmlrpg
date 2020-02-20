e7e3829341f89531d1d71c2d5a4dbbfb112ec822
ADodman <a.dodman@steinberg.de>
2020-02-20 17:35:37 +0000

    Added text rendering and display scale

---
+++ lib/include/point.h
@@ -0,0 +1,12 @@
+//
+//  point.h
+//  prpg
+//
+//  Created by ADodman on 20/02/2020.
+//
+
+#ifndef point_h
+#define point_h
+
+
+#endif /* point_h */
---
+++ lib/include/size.h
@@ -0,0 +1,12 @@
+//
+//  size.h
+//  prpg
+//
+//  Created by ADodman on 20/02/2020.
+//
+
+#ifndef size_h
+#define size_h
+
+
+#endif /* size_h */
